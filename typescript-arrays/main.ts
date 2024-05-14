a98f5440149bd54cae34490d548604ebeec463fc
Aaditya Desai <aadityadesai09@gmail.com>
2024-05-14 20:45:17 +0000

    Fixed changes in typescript-arrays

--- typescript-arrays/main.ts
+++ typescript-arrays/main.ts
@@ -1,4 +1,4 @@
-const colors: any[] = ['red', 'white', 'blue'];
+const colors: string[] = ['red', 'white', 'blue'];
 
 console.log('value of colors[0]:', colors[0]);
 console.log('value of colors[1]:', colors[1]);
@@ -15,12 +15,12 @@ console.log('Mexico is', mexico);
 console.log('value of colors:', colors);
 console.log('typeof colors:', typeof colors);
 
-const students: any[] = ['Frank', 'Bob', 'Chris', 'David'];
+const students: string[] = ['Frank', 'Bob', 'Chris', 'David'];
 
 const numberOfStudents: number = students.length;
 console.log(`There are ${numberOfStudents} students in the class`);
 const lastIndex: number = numberOfStudents - 1;
-const lastStudent: number = students[lastIndex];
+const lastStudent: string = students[lastIndex];
 console.log(`The last student in the array is ${lastStudent}`);
 console.log('value of students:', students);
 console.log('typeof students:', typeof students);
