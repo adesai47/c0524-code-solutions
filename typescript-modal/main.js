a9b4ca245bc5de8a81efd4933ae653c456834932
Aaditya Desai <aadityadesai09@gmail.com>
2024-05-29 21:34:27 +0000

    Completed typescript-modal

---
+++ typescript-modal/main.js
@@ -0,0 +1,13 @@
+'use strict';
+const $open = document.querySelector('.open-modal');
+const $dismiss = document.querySelector('.dismiss-modal');
+const $dialogElement = document.querySelector('dialog');
+if (!$open) throw new Error('The $open query failed');
+if (!$dismiss) throw new Error('The $dismiss query failed');
+if (!$dialogElement) throw new Error('The $dialog query failed');
+$open.addEventListener('click', () => {
+  $dialogElement.showModal();
+});
+$dismiss.addEventListener('click', () => {
+  $dialogElement.close();
+});
