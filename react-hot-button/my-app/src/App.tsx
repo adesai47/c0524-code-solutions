850251247a682b471ad1ea9865da65c6adae85aa
Aaditya Desai <aadityadesai09@gmail.com>
2024-07-10 21:42:44 +0000

    Completed react-hot-button

---
+++ react-hot-button/my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { HotButton } from './HotButton';
+
+function App() {
+  return (
+    <div className="App">
+      <HotButton label="Hot Button" />
+    </div>
+  );
+}
+
+export default App;
