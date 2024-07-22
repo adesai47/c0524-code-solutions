5ea329a673fbeb3f5c3bb079994361ba8692d473
Aaditya Desai <aadityadesai09@gmail.com>
2024-07-22 15:50:43 +0000

    finished phase 3

--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Banner from './Banner';
 import NextButton from './NextButton';
 import PrevButton from './PrevButton';
